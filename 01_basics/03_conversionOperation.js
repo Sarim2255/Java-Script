850c1a3cfe946fe7e46d8d9b1f1d4f1644c38b7c
Sarim2255 <167195645+Sarim2255@users.noreply.github.com>
2025-11-02 15:59:07 +0000

    conversion operation done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,3 +14,21 @@ console.log(typeof(score2));
 let valueInNumber2 = Number(score2)
 console.log(typeof valueInNumber2);
 console.log(valueInNumber2);
+
+/*
+"33"=>33
+"33abc" =>NaN
+true => 1; false => 0
+*/
+
+let isLoggedIn = "SAM"
+let booleanIsLoggedein = Boolean(isLoggedIn)
+console.log(booleanIsLoggedein)
+/*
+1 =>true; 0=>false
+"sam"=>true
+*/
+let num = 33
+let strNum = String(num)
+console.log(strNum)
+console.log(typeof strNum)
