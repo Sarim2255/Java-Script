a1528790172d32780e87c35a78c5fb073ea7011e
Sarim2255 <167195645+Sarim2255@users.noreply.github.com>
2025-11-02 16:19:53 +0000

    conversion operation done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -5,7 +5,6 @@ console.log(typeof(score1));
 let valueInNumber1 = Number(score1)
 console.log(typeof valueInNumber1);
 console.log(valueInNumber1);
-
 //With another value which is alphanumberic
 let score2 = "33abc"
 console.log(typeof score2);
@@ -32,3 +31,38 @@ let num = 33
 let strNum = String(num)
 console.log(strNum)
 console.log(typeof strNum)
+
+// *****************************Operations************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**3);
+console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2
+console.log(str3);
+console.log("1"+2);     //output->12
+console.log("1"+2+2);   //output->122
+console.log(1+"2");     //output->12
+console.log(1+2+"2");   //output->32
+console.log(+true)
+console.log(+"")
+// ----------------------------------------------------------------------------
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+// ----------------------------------------------------------------------------
+let gameCounter = 100
+++gameCounter
+// gameCounter++
+console.log(gameCounter)
+
+// for relevance the link is provided below
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
